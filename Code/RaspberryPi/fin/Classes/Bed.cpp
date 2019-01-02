736ebd4ef5f2767909c5c6e986829139228c989f
Leacyn <vincent.geers@ziggo.nl>
2019-01-02 17:16:01 +0100

    fixes to map iteration

--- Code/RaspberryPi/fin/Classes/Bed.cpp
+++ Code/RaspberryPi/fin/Classes/Bed.cpp
@@ -22,9 +22,9 @@ Bed::~Bed(){
 std::map Bed::check(){
     std::map<int id, int val> returnmap;
     for(std::map<int, int>::iterator i = IO.begin(); i!=IO.end(); ++i){
-      int check = device.sendRead(i)
+      int check = device.sendRead(i->first)
       if(check != IO[i]){
-        returnmap[i] = check;
+        returnmap[i->first] = check;
       }
     }
       return returnmap;
