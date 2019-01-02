20c5e56d135e00730b1f3fb8beb6cdf51adb1838
programmer <programmer@oopr.localdomain>
2019-01-02 14:44:24 +0100

    domotica.h changed

--- Code/RaspberryPi/Classes/Domotica.cpp
+++ Code/RaspberryPi/Classes/Domotica.cpp
@@ -3,17 +3,40 @@
   version: 0.1
   contributors:
   Vincent Geers 13009672
-----------------------------------*/
-#include "Domotica.h"
+  Stijn van Es 17018498
+  ----------------------------------*/
 
 
 
-Domotica::Domotica(){
+int const maxSensor = 3;
 
-}
+using namespace std;
 
+int main(int argc, char** argv){
+	/*SETUP*/
+	cout << endl;
 
-Domotica::~Domotica(){
+	/*set up connection to database*/
+//	DataBase sql(PATH, USER, PASSWD, DB);
+	/*set up connection to WEMOS*/
+	TCP wemos(wemosAddress, WEMOS_PORT);
 
+	/*LOOP*/
+//	while(1){
+//	for(int i = 1; i <= maxSensor; i++){
+//		if (int val = sql.queryValue("stateVal", "Sensor", i) != sql.queryValue("prevVal", "Sensor", i)){
+//			sql.updateValue("Sensor", "prevVal", val, i);
+//			//wemos.sendMsg("toggle");
+//		}
+//	}
+//	}
 
+	//sql.addUser("Willem", "Welkom123");
+	//sql.queryUser();
+
+//	sql.closeConnection();
+	wemos.sendMsg((char *) "Hello from client");
+	cout << wemos.receive() << endl;
+
+	return 0;
 }
---
+++ Code/RaspberryPi/Classes/Domotica.h
@@ -0,0 +1,19 @@
+/*-----------------------------------
+  Domotica class definition
+  version: 0.1
+  contributors:
+  Vincent Geers 13009672
+  Stijn van Es 17018498
+----------------------------------*/
+
+/* C++ includes */
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "DataBase.cpp"
+
+/*Define SQL login data*/
+#define PATH "tcp://127.0.0.1:3306"
+#define USER "monitor"
+#define PASSWD "100%Domotica"
+#define DB "domotics"
