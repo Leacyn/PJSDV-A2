f3f475a9ce3ec33d52dcacbf41d4dbc167478056
Leacyn <vincent.geers@ziggo.nl>
2019-01-02 17:02:02 +0100

    created returnmap

--- Code/RaspberryPi/fin/Classes/Bed.cpp
+++ Code/RaspberryPi/fin/Classes/Bed.cpp
@@ -1,6 +1,6 @@
 /*-----------------------------------
   Bed class definition
-  version: 0.1
+  version: 0.3
   contributors:
   Vincent Geers 13009672
   Stijn van Es 17018498
@@ -10,11 +10,22 @@
 
 
 Bed::Bed(char *ipaddress):Device(ipaddress){
-  IO = std::map<int, std::string,int> {{1,"Sensor",0},{2,"Actuator",0},{3,"Sensor",0}};
+  IO = std::map<int id, int prevVal> {{1,0},{2,0},{3,0}};
 }
 
 
 Bed::~Bed(){
 
 
+}
+
+std::map Bed::check(){
+    std::map<int id, int val> returnmap;
+    for(std::map::iterator i = IO.begin(); i!=IO.end(); ++i)
+      int check = device.sendRead(i)
+      if(check != IO[i]){
+        returnmap[i] = check;
+      }
+
+      return returnmap;
 }
