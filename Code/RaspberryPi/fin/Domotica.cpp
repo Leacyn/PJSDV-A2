96805502a001bbc3fa506bfde8774d64a3be1e6f
Stijn-FE <stijn.f98@gmail.com>
2019-01-05 16:40:03 +0100

    -

--- Code/RaspberryPi/fin/Domotica.cpp
+++ Code/RaspberryPi/fin/Domotica.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv){
     deviceAmount++;
     Device dev(d.ipAddress.c_str(), d.name,d.IDs);
     for (int id : d.IDs){
-      deviceIDs[id]=dev;
+      deviceIDs[id] = dev;
     }
     devices[d.name]=dev;
 	}
@@ -35,13 +35,13 @@ int main(int argc, char** argv){
 	while(1){
     if(sql.checkStateChange()){/*Returns true when changes have been made from website*/
       for(map<int,int>::iterator it = sql.changes.begin(); it != sql.changes.end(); ++it){/*all changes are set to the sql.changes map with id, value*/
-        devices[it->first].changeValue(it->first,it->second);	/*For every change in database, change value of device*/
+        deviceIDs[it->first].changeValue(it->first,it->second);	/*For every change in database, change value of device*/
         clog << "value changed ID:'" << it->first << "' Value:'" << it->second << "'" << endl;
       }
     }
 
 
-    for(map<int, Device>::iterator it = deviceIDs.begin(); it!=deviceIDs.end(); ++it){/*for each device*/
+    for(map<string, Device>::iterator it = devices.begin(); it!=devices.end(); ++it){/*for each device*/
       changes = it->second.check();
       for(map<int,int>::iterator i = changes.begin(); i!=changes.end(); ++i){/*for each sensor/ actuator*/
         sql.setPrevValSensor(i->first, i->second);
